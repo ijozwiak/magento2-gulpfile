2fa4420843772198447ba6ba767af108d9443920
Jakub Kulig <jakub.kulig@rocketweb.com>
2019-02-12 13:53:30 +0100

    Update stylelint rules

--- dev/tools/gulp/configs/stylelint.js
+++ dev/tools/gulp/configs/stylelint.js
@@ -7,9 +7,9 @@ module.exports = {
         "indentation": 4,
         "string-quotes": "single",
         "selector-max-id": 0,
-        "selector-class-pattern": "^_?[a-z0-9]+(-[a-z0-9]+)*$",
         "max-nesting-depth": 4,
         "number-leading-zero": "never",
-        "max-empty-lines": 2
+        "max-empty-lines": 2,
+        "font-family-no-missing-generic-family-keyword": null
     }
 }
